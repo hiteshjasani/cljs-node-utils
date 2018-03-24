6d50ea5737993630ba7aa7a8567b9df470be8498
Hitesh Jasani <hitesh.jasani@gmail.com>
2018-03-23 21:01:23 -0400

    Refactor tests to investigate issues

--- Makefile
+++ Makefile
@@ -1,8 +1,14 @@
 
-compile:
+test: compile-test-node
+	node target/test-node.js
+
+compile-test-node:
+	lein cljsbuild once test-node
+
+compile-all:
 	lein cljsbuild once
 
-test: compile
+test-all: compile
 	node target/test-node.js
 	node target/test-simple.js
 	node target/test-adv.js
--- src/test/cnu/interop_test.cljs
+++ src/test/cnu/interop_test.cljs
@@ -1,12 +1,130 @@
 (ns cnu.interop-test
-  (:require [cljs.test :refer [deftest testing is]]
+  (:require [cljs.test :refer [deftest testing is are]]
             [cnu.interop :refer [jsobj->edn json->edn]]))
 
 (deftest js-object-to-edn
-  (testing "string keys"
+  (testing "jsobj->edn default keywordize keys conversion"
+    (are [exp act] (= exp (jsobj->edn act true))
+      {:foo "bar"} (clj->js {:foo "bar"})
+      {:foo "bar"} (clj->js {"foo" "bar"})
+      {:foo "bar"} #js {:foo "bar"}
+      {:foo "bar"} #js {"foo" "bar"}
+      ))
+
+  (testing "json->edn default keywordize keys conversion"
+    (are [exp act] (= exp (json->edn act true))
+      {:foo "bar"} (clj->js {:foo "bar"})
+      {:foo "bar"} (clj->js {"foo" "bar"})
+      {:foo "bar"} #js {:foo "bar"}
+      {:foo "bar"} #js {"foo" "bar"}
+      ))
+
+  (testing "js->clj keywordize keys conversion - for comparison only"
+    (are [exp act] (= exp (js->clj act :keywordize-keys true))
+      {:foo "bar"} (clj->js {:foo "bar"})
+      {:foo "bar"} (clj->js {"foo" "bar"})
+      {:foo "bar"} #js {:foo "bar"}
+      {:foo "bar"} #js {"foo" "bar"}
+      ))
+  )
+
+(deftest json-to-edn
+  (testing "json->edn default keywordize keys conversion"
+    (are [exp act] (= exp (json->edn act true))
+      [{:foo "bar"}] (clj->js [{:foo "bar"}])
+      [{:foo "bar"}] (clj->js [{"foo" "bar"}])
+      [{:foo "bar"}] #js [{:foo "bar"}]
+      ;; [{:foo "bar"}] #js [{"foo" "bar"}]
+      ))
+
+  (testing "json->edn (no force keywordizing) conversion"
+    (are [exp act] (= exp (json->edn act false))
+      ;; [{:foo "bar"}] (clj->js [{:foo "bar"}])
+      [{"foo" "bar"}] (clj->js [{"foo" "bar"}])
+      [{:foo "bar"}]  #js [{:foo "bar"}]
+      [{"foo" "bar"}] #js [{"foo" "bar"}]
+      ))
+
+  (testing "js->clj keywordize keys conversion - for comparison only"
+    (are [exp act] (= exp (js->clj act :keywordize-keys true))
+      [{:foo "bar"}] (clj->js [{:foo "bar"}])
+      [{:foo "bar"}] (clj->js [{"foo" "bar"}])
+      [{:foo "bar"}] #js [{:foo "bar"}]
+      ;; [{:foo "bar"}] #js [{"foo" "bar"}]
+      ))
+
+  (testing "js->clj (no force keywordizing) conversion - for comparison only"
+    (are [exp act] (= exp (js->clj act))
+      ;; [{:foo "bar"}] (clj->js [{:foo "bar"}])
+      [{"foo" "bar"}] (clj->js [{"foo" "bar"}])
+      [{:foo "bar"}]  #js [{:foo "bar"}]
+      [{"foo" "bar"}] #js [{"foo" "bar"}]
+      ))
+  )
+
+(deftest testing-cljs-core-library-conversions
+  (testing "No keywordizing"
+    (testing "Roundtrip clj->js"
+      (testing "Works as expected"
+        (are [e] (= e (-> e clj->js js->clj))
+          {"foo" "bar"}
+          [{"foo" "bar"}]
+          ))
+      (testing "Unexpected behavior"
+        (are [e] (= e (-> e clj->js js->clj))
+          {:foo "bar"}
+          [{:foo "bar"}]
+          ))
+      )
+    (testing "Convert #js"
+      (testing "Works as expected"
+        (are [exp jsliteral] (= exp (js->clj jsliteral))
+          {"foo" "bar"}   #js {"foo" "bar"}
+          [{"foo" "bar"}] #js [{"foo" "bar"}]
+          [{:foo "bar"}]  #js [{:foo "bar"}]
+          ))
+      (testing "Unexpected behavior"
+        (are [exp jsliteral] (= exp (js->clj jsliteral))
+          {:foo "bar"} #js {:foo "bar"}
+          ))
+      ))
+  (testing "Forced keywordizing"
+    (testing "Roundtrip clj->js"
+      (testing "Works as expected"
+        (are [exp x] (= exp (-> x clj->js (js->clj :keywordize-keys true)))
+          {:foo "bar"}   {"foo" "bar"}
+          {:foo "bar"}   {:foo "bar"}
+          [{:foo "bar"}] [{"foo" "bar"}]
+          [{:foo "bar"}] [{:foo "bar"}]
+          ))
+      #_(testing "Unexpected behavior"
+          (are [exp x] (= exp (-> x clj->js (js->clj :keywordize-keys true)))
+            ))
+      )
+    (testing "Convert #js"
+      (testing "Works as expected"
+        (are [exp jsliteral] (= exp (js->clj jsliteral :keywordize-keys true))
+          {:foo "bar"}   #js {"foo" "bar"}
+          {:foo "bar"}   #js {:foo "bar"}
+          [{:foo "bar"}] #js [{:foo "bar"}]
+          ))
+      (testing "Unexpected behavior"
+        (are [exp jsliteral] (= exp (js->clj jsliteral :keywordize-keys true))
+          [{:foo "bar"}] #js [{"foo" "bar"}]
+          ))
+      ))
+  )
+
+#_(deftest js-object-to-edn
+  (testing "string keys clj->js"
     (is (= {"foo" "bar"} (jsobj->edn (clj->js {:foo "bar"}) false)))
     (is (= {"foo" "bar"} (json->edn (clj->js {:foo "bar"}) false)))
     (is (= {"foo" "bar"} (js->clj (clj->js {:foo "bar"}))))
+    )
+  (testing "string keys #js"
+    (is (= {"foo" "bar"} (jsobj->edn #js {:foo "bar"}) false))
+    (is (= {"foo" "bar"} (json->edn #js {:foo "bar"}) false))
+    (is (= {"foo" "bar"} (js->clj #js {:foo "bar"})))
     )
   (testing "keyword keys"
     (is (= {:foo "bar"} (jsobj->edn (clj->js {:foo "bar"}))))
@@ -14,7 +132,7 @@
     (is (= {:foo "bar"} (js->clj (clj->js {:foo "bar"}) :keywordize-keys true)))
     ))
 
-(deftest json-to-edn
+#_(deftest json-to-edn
   (testing "string keys"
     (is (= [{"foo" "bar"}] (json->edn (clj->js [{:foo "bar"}]) false)))
     (is (= [{"foo" "bar"}] (js->clj (clj->js [{:foo "bar"}]) :keywordize-keys false)))
